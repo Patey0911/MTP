b0033a24ffe404d54cda25c6b1fc590fa5e16838
Patey0911 <44579714+Patey0911@users.noreply.github.com>
2022-05-12 20:44:41 +0300

    Commit

---
+++ Echipamente_Institutie/Echipamente_Institutie/Echipament.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Echipamente_Institutie
+{
+    internal class Echipament
+    {
+        private int nr;
+        private string model, producator, categorie, nume_persoana, cnp;
+        [Description("Nr Inventar"), Category("Detalii echipament")]
+        public int Nr { get { return nr; } }
+        [Description("Model echipament"),Category("Detalii echipament")]
+        public string Model { get { return model; } }
+        [Description("Producator echipament"), Category("Detalii echipament")]
+        public string Producator { get { return producator; } }
+        [Description("Categorie"), Category("Detalii echipament")]
+        public string Categorie { get { return categorie; } }
+        [Description("Nume Persoana"), Category("Detalii persoana")]
+        public string Nume_persoana { get { return nume_persoana; } }
+        [Description("CNP"), Category("Detalii persoana")]
+        public string Cnp { get { return cnp; } }
+
+        public Echipament(int nr, string model, string producator, string categorie, string nume_persoana, string cnp)
+        {
+            this.nr = nr;
+            this.model = model;
+            this.producator = producator;
+            this.categorie = categorie;
+            this.nume_persoana = nume_persoana;
+            this.cnp = cnp;
+        }
+    }
+}
---
+++ Echipamente_Institutie/Echipamente_Institutie/Form1.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Echipamente_Institutie
+{
+    public partial class Form1 : Form
+    {
+        private int nr;
+        private string model, producator, categorie, nume_persoana, cnp;
+        string nume_ret;
+
+        List<Echipament> lista = new List<Echipament>();
+        public Form1()
+        {
+            InitializeComponent();
+            string dir = @"C:\Users\mihal\Desktop\MTP\Echipamente_Institutie\Echipamente_Institutie\Salvare_Fisier\Calculator";
+            if (!Directory.Exists(dir))
+            {
+                Directory.CreateDirectory(dir);
+            }
+            dir = @"C:\Users\mihal\Desktop\MTP\Echipamente_Institutie\Echipamente_Institutie\Salvare_Fisier\Laptop";
+            if (!Directory.Exists(dir))
+            {
+                Directory.CreateDirectory(dir);
+            }
+            dir = @"C:\Users\mihal\Desktop\MTP\Echipamente_Institutie\Echipamente_Institutie\Salvare_Fisier\Imprimanta";
+            if (!Directory.Exists(dir))
+            {
+                Directory.CreateDirectory(dir);
+            }
+            dir = @"C:\Users\mihal\Desktop\MTP\Echipamente_Institutie\Echipamente_Institutie\Salvare_Fisier\Scaner";
+            if (!Directory.Exists(dir))
+            {
+                Directory.CreateDirectory(dir);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            nr = (int)numericUpDown1.Value;
+            model = textBox1.Text;
+            producator = textBox2.Text;
+            categorie = comboBox1.Text;
+            nume_persoana = textBox3.Text;
+            cnp = textBox4.Text;
+            if (cnp.Length == 13)
+            {
+                Echipament a = new Echipament(nr, model, producator, categorie, nume_persoana, cnp);
+                lista.Add(a);
+                Button btn = new Button();
+                btn.Text = a.Model;
+                btn.Width = 180;
+                btn.Height = 30;
+                //btn.Tag = Echipament;
+                if (a.Categorie.Contains("Calculator"))
+                    flowLayoutPanel1.Controls.Add(btn);
+                if (a.Categorie.Contains("Laptop"))
+                    flowLayoutPanel2.Controls.Add(btn);
+                if (a.Categorie.Contains("Imprimanta"))
+                    flowLayoutPanel3.Controls.Add(btn);
+                if (a.Categorie.Contains("Scaner"))
+                    flowLayoutPanel4.Controls.Add(btn);
+                btn.Click += Btn_Click;
+            }
+            else
+            {
+                MessageBox.Show("CNP-ul nu are 13 cifre !!!");
+                errorProvider1.SetError(textBox4, "CNP-ul nu are 13 cifre !!!");
+            }
+        }
+        void Btn_Click(object sender, EventArgs e)
+        {
+            Button btn = (Button)sender;
+            Echipament j = (Echipament)btn.Tag;
+            foreach (var echip in lista)
+            {
+                if(echip.Model==btn.Text)
+                {
+                    propertyGrid1.SelectedObject = echip;
+                    nume_ret=echip.Model;
+                }
+            }
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            foreach (var echip in lista)
+            {
+                if (echip.Model == nume_ret)
+                {
+                    string path = @"C:\Users\mihal\Desktop\MTP\Echipamente_Institutie\Echipamente_Institutie\Salvare_Fisier\"+echip.Categorie+"\\"+echip.Model+".txt";
+                    using (StreamWriter sw = File.CreateText(path))
+                    {
+                        sw.WriteLine("Nr. Inventar: " + echip.Nr);
+                        sw.WriteLine("Model echipament: " + echip.Model);
+                        sw.WriteLine("Producator: " + echip.Producator);
+                        sw.WriteLine("Producator: " + echip.Producator);
+                        sw.WriteLine("Categorie: " + echip.Categorie);
+                        sw.WriteLine("Nume Persoana: " + echip.Nume_persoana);
+                        sw.WriteLine("CNP: " + echip.Cnp);
+                    }
+                }
+            }
+        }
+    }
+}
